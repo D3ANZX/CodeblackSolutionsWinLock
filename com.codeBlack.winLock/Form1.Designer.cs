27cffb9f60e41051955246289fde8de217805a56
D3ANZX <gasflorentinodean@gmail.com>
2026-03-10 15:04:48 +0800

    Initial Cryptographic Service

--- com.codeBlack.winLock/Form1.Designer.cs
+++ com.codeBlack.winLock/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace com.codeBlack.winLock
 {
-    partial class Form1
+    partial class WinLock
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,33 +28,125 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WinLock));
+            encryptButton = new Button();
+            textBox1 = new TextBox();
+            decryptButton = new Button();
             label1 = new Label();
+            pictureBox1 = new PictureBox();
+            label2 = new Label();
+            label3 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
+            // 
+            // encryptButton
+            // 
+            encryptButton.BackColor = Color.FromArgb(0, 0, 64);
+            encryptButton.ForeColor = Color.White;
+            encryptButton.Location = new Point(577, 134);
+            encryptButton.Name = "encryptButton";
+            encryptButton.Size = new Size(203, 93);
+            encryptButton.TabIndex = 0;
+            encryptButton.Text = "EncryptFile";
+            encryptButton.UseVisualStyleBackColor = false;
+            encryptButton.Click += encryptButton_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(338, 200);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(209, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // decryptButton
+            // 
+            decryptButton.BackColor = Color.FromArgb(0, 0, 64);
+            decryptButton.BackgroundImageLayout = ImageLayout.Center;
+            decryptButton.ForeColor = Color.White;
+            decryptButton.Location = new Point(577, 250);
+            decryptButton.Name = "decryptButton";
+            decryptButton.Size = new Size(203, 104);
+            decryptButton.TabIndex = 2;
+            decryptButton.Text = "DecryptFile";
+            decryptButton.UseVisualStyleBackColor = false;
+            decryptButton.Click += decryptButton_Click;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(239, 75);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(338, 170);
             label1.Name = "label1";
-            label1.Size = new Size(50, 20);
-            label1.TabIndex = 0;
-            label1.Text = "label1";
+            label1.Size = new Size(113, 20);
+            label1.TabIndex = 3;
+            label1.Text = "Decryption Key:";
             label1.Click += label1_Click;
             // 
-            // Form1
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 23);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(109, 104);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(136, 29);
+            label2.Name = "label2";
+            label2.Size = new Size(195, 28);
+            label2.TabIndex = 5;
+            label2.Text = "Code Black Solutions";
+            label2.Click += label2_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(127, 57);
+            label3.Name = "label3";
+            label3.Size = new Size(283, 81);
+            label3.TabIndex = 6;
+            label3.Text = "WinLOCK";
+            // 
+            // WinLock
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(0, 0, 64);
+            BackgroundImageLayout = ImageLayout.Center;
             ClientSize = new Size(833, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(pictureBox1);
             Controls.Add(label1);
-            Name = "Form1";
-            Text = "Form1";
+            Controls.Add(decryptButton);
+            Controls.Add(textBox1);
+            Controls.Add(encryptButton);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MdiChildrenMinimizedAnchorBottom = false;
+            Name = "WinLock";
+            Text = "WinLOCK";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
+        private Button encryptButton;
+        private TextBox textBox1;
+        private Button decryptButton;
         private Label label1;
+        private PictureBox pictureBox1;
+        private Label label2;
+        private Label label3;
     }
 }
--- com.codeBlack.winLock/Form1.cs
+++ com.codeBlack.winLock/Form1.cs
@@ -1,15 +1,116 @@
+using System.Security.Cryptography;
+using System;
+using System.IO;
+using System.Text;
+
+
 namespace com.codeBlack.winLock
 {
-    public partial class Form1 : Form
+    public partial class WinLock : Form
     {
-        public Form1()
+        const string EncrFolder = @"c:\EncrFolder\";
+        const string DecrFolder = @"c:\DecrFolder\";
+        const string SrcFolder = @"c:\Desktop\";
+        public WinLock()
         {
             InitializeComponent();
         }
 
+
+        //Function for encryptButton Click Event
+        private void encryptButton_Click(object sender, EventArgs e)
+        {   //Create a new instance of OpenFile Dialog
+            string salt = "test";
+            int iterations = 1000;
+            OpenFileDialog _encryptOpenFileDialog = new OpenFileDialog();
+            _encryptOpenFileDialog.InitialDirectory = SrcFolder;
+
+            if (_encryptOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {   //Execute the EncryptFile() function
+                EncryptFile(_encryptOpenFileDialog.FileName, textBox1.Text, salt, iterations);
+            }
+        }
+
+        private void decryptButton_Click(object sender, EventArgs e)
+        {
+            string salt = "test";
+            int iterations = 1000;
+            OpenFileDialog _encryptOpenFileDialog = new OpenFileDialog();
+            _encryptOpenFileDialog.InitialDirectory = SrcFolder;
+
+            if (_encryptOpenFileDialog.ShowDialog() == DialogResult.OK)
+            {   //Execute the EncryptFile() function
+                DecryptFile(_encryptOpenFileDialog.FileName, textBox1.Text, salt, iterations);
+            }
+        }
+
+        private void EncryptFile(string file, string password, string salt, int iterations)
+        {
+            //Convert string parameters to bytes
+            byte[] saltBytes = Encoding.UTF8.GetBytes(salt); //Convert salt to bytes
+            byte[] key = Rfc2898DeriveBytes.Pbkdf2(password, saltBytes, iterations, HashAlgorithmName.SHA256, outputLength: 32); //set Key to R2898 Bytes
+
+            //Create Aes objet and instance
+            using (Aes aes = Aes.Create())
+            {
+                //Assign the RFC2898Derive Bytes value as the aes.key
+                aes.Key = key;
+                aes.IV = new byte[16];
+
+
+
+                using (FileStream output = new FileStream(Path.Combine(EncrFolder, Path.ChangeExtension(file, ".enc")), FileMode.Create, FileAccess.Write, FileShare.ReadWrite))
+                using (CryptoStream stream = new CryptoStream(output, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    new FileStream(file, FileMode.Open).CopyTo(stream);
+                }
+                MessageBox.Show("File Encrypted successfully");
+            }
+        }
+
+        private void DecryptFile(string file, string password, string salt, int iterations)
+        {
+            //Convert string parameters to bytes
+            byte[] saltBytes = Encoding.UTF8.GetBytes(salt); //Convert salt to bytes
+            byte[] key = Rfc2898DeriveBytes.Pbkdf2(password, saltBytes, iterations, HashAlgorithmName.SHA256, outputLength: 32); //set Key to R2898 Bytes
+
+            //Create Aes objet and instance
+            try
+            {
+                using (Aes aes = Aes.Create())
+                {
+                    //Assign the RFC2898Derive Bytes value as the aes.key
+                    aes.Key = key;
+                    aes.IV = new byte[16];
+
+                    using (FileStream input = new FileStream(file, FileMode.Open, FileAccess.Read))
+                    using (FileStream output = new FileStream(Path.Combine(EncrFolder, Path.ChangeExtension(file, ".pdf")), FileMode.Create, FileAccess.Write, FileShare.ReadWrite))
+                    using (CryptoStream stream = new CryptoStream(input, aes.CreateDecryptor(), CryptoStreamMode.Read))
+                    {
+                        stream.CopyTo(output);
+                    }
+                    MessageBox.Show("File Decrypted successfully");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Invalid Key");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
         private void label1_Click(object sender, EventArgs e)
         {
-            Nigga
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
